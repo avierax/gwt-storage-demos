1d680afc8c007b41f7c1aab5e7a20e2f8da6e3ed
Ariel Viera <ariel.viera@gmail.com>
2017-12-28 11:25:49 -0500

    Polish the app code

--- gwt-storage-demo-client/src/main/java/com/tissca/gwttests/App.java
+++ gwt-storage-demo-client/src/main/java/com/tissca/gwttests/App.java
@@ -2,46 +2,52 @@ package com.tissca.gwttests;
 
 import com.google.gwt.core.client.EntryPoint;
 import elemental2.core.JsDate;
-import elemental2.dom.DomGlobal;
-import elemental2.dom.Element;
-import elemental2.dom.Event;
-import elemental2.dom.EventListener;
-import elemental2.dom.HTMLInputElement;
-import org.apache.tapestry.wml.Do;
+import elemental2.dom.*;
 import org.gwtproject.storage.client.Storage;
-
-import java.util.Date;
+import static elemental2.dom.DomGlobal.*;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class App implements EntryPoint {
 
+	private final Storage localStorage = Storage.getLocalStorageIfSupported();
+	private final Storage sessionStorage = Storage.getSessionStorageIfSupported();
+
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		DomGlobal.window.console.log("version 0.2");
+		window.console.log("version 0.3");
 
 		loadPreviouslyStoredValues();
 
-		DomGlobal.document.querySelector("#session").addEventListener("keyup", new EventListener() {
+		document.querySelector("#session").addEventListener("keyup", new EventListener() {
 			public void handleEvent(Event evt) {
-				Storage.getSessionStorageIfSupported().setItem("value", ((HTMLInputElement) evt.target).value);
-				Storage.getSessionStorageIfSupported().setItem("timestamp", String.valueOf(JsDate.now()));
+				sessionStorage.setItem("value", ((HTMLInputElement) evt.target).value);
+				sessionStorage.setItem("timestamp", String.valueOf(JsDate.now()));
 			}
 		});
 
-		DomGlobal.document.querySelector("#local").addEventListener("keyup", new EventListener() {
+		document.querySelector("#local").addEventListener("keyup", new EventListener() {
 			public void handleEvent(Event evt) {
-				Storage.getLocalStorageIfSupported().setItem("value", ((HTMLInputElement) evt.target).value);
-				Storage.getLocalStorageIfSupported().setItem("timestamp", String.valueOf(JsDate.now()));
+				localStorage.setItem("value", ((HTMLInputElement) evt.target).value);
+				localStorage.setItem("timestamp", String.valueOf(JsDate.now()));
+			}
+		});
+
+		document.querySelector("#clear").addEventListener("click", new EventListener() {
+			public void handleEvent(Event evt) {
+				sessionStorage.clear();
+				localStorage.clear();
+				loadPreviouslyStoredValues();
 			}
 		});
 	}
 
 	private void loadPreviouslyStoredValues() {
 		String[] storageNames = new String[] {"local", "session"};
+		document.querySelector("#previous").innerHTML = "";
 		for(String storageName:storageNames) {
 			Storage storage = storageName.equals("local") ? Storage.getLocalStorageIfSupported() : Storage.getSessionStorageIfSupported();
 			String value = storage.getItem("value");
@@ -50,10 +56,9 @@ public class App implements EntryPoint {
 				double delta = (JsDate.now() - Double.parseDouble(storage.getItem("timestamp"))) / 1000;
 				formattedString = storageName + "Storage: " + value + " (last updated: " + delta + "s ago)";
 			}
-			Element li = DomGlobal.document.createElement("li");
+			Element li = document.createElement("li");
 			li.innerHTML = formattedString;
-			DomGlobal.document.querySelector("#previous").appendChild(li);
-
+			document.querySelector("#previous").appendChild(li);
 		}
 	}
 }
